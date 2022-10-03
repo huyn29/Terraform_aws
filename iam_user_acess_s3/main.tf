4660524d382db2cf8efbf991c36306903fba3a78
huyn29 <nvh1892k@gmail.com>
2022-10-03 23:48:00 +0700

    change 1

---
+++ iam_user_acess_s3/main.tf
@@ -0,0 +1,103 @@
+provider "aws" {
+  region = "us-east-1"
+}
+variable "bucket_name" {
+  type = string
+  default = "hcl-demo-test"
+}
+
+locals {
+  map= {
+    //princilpals
+    princilpals_dynamic = {
+    "1"={
+      type = "AWS"
+      identifiers = ["704063666843"],
+    }
+    "2"={
+      type = "AWS"
+      identifiers = ["878103297030"],
+    }
+    }
+    //statement
+     statement_dynamic = {
+    "1" ={
+       actions = ["s3:ListAllMyBuckets",]
+       effect = "Allow"
+       resources = ["*",]
+    }
+    "2" ={
+       actions = [ "s3:ListBucket",
+                "s3:GetBucketLocation",]
+        effect = "Allow"
+       resources = ["arn:aws:s3:::${var.bucket_name}",]
+    }
+    "3" ={
+       actions = [  "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",]
+        effect = "Allow"
+       resources = ["arn:aws:s3:::${var.bucket_name}/*",]
+    }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ip_document" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    dynamic "principals" {
+      for_each = local.map.princilpals_dynamic
+      content {
+        type        = principals.value.type
+        identifiers = principals.value.identifiers
+      } 
+    }
+  }
+}
+resource "aws_iam_role" "role" {
+  name                = "ThangUpdateApp"
+  assume_role_policy = data.aws_iam_policy_document.ip_document.json
+  managed_policy_arns = [aws_iam_policy.ip.arn]
+}
+
+data "aws_iam_policy_document" "ip_document2" {
+
+  dynamic "statement" {
+    for_each = local.map.statement_dynamic
+
+    content{
+      actions = statement.value.actions
+      effect = statement.value.effect
+      resources = statement.value.resources
+    }
+  }
+}
+resource "aws_iam_policy" "ip" {
+    name = "AccessRead"
+    path = "/"
+    description = "access to S3 bucket"
+
+    policy = data.aws_iam_policy_document.ip_document2.json
+}
+
+resource "aws_iam_user" "iu" {
+    name = "ThangAccess"
+    path = "/system/"
+}
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "accessS3user"
+  user = aws_iam_user.iu.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Resource": "arn:aws:iam::878103297030:role/ThangUpdateApp"
+    }
+  }
+EOF
+}
